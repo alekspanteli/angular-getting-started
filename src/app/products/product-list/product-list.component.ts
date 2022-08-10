32a9aadf5e285052add62ef90af1832819cf189e
alekspanteli <alekspanteli@rambler.ru>
2022-08-10 14:23:50 +0300

    add ngmodel to forms

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -10,6 +10,7 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 20;
   showImage: boolean = false;
+  listFilter: string = 'cart';
   products: any[] = [
     {
       productId: 1,
