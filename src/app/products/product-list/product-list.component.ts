f9868c43be4ef4f5ca8e7f9ff8c910cf9a293a80
alekspanteli <alekspanteli@rambler.ru>
2022-08-11 18:12:23 +0300

    add interface

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../products';
 
 @Component({
   selector: 'pm-products',
@@ -11,7 +12,7 @@ export class ProductListComponent implements OnInit {
   imageMargin: number = 20;
   showImage: boolean = false;
   listFilter: string = 'cart';
-  products: any[] = [
+  products: IProduct[] = [
     {
       productId: 1,
       productName: 'Leaf Rake',
